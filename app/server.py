75e05c35520178daba537b741abaea90dcf4af5e
Elijah Hoole <elihoole@gmail.com>
2020-01-01 08:24:16 +0530

    changed model, classes, and static display

--- app/server.py
+++ app/server.py
@@ -7,9 +7,11 @@ from io import BytesIO
 from fastai.vision import *
 import base64
 
-model_file_url = 'https://github.com/pankymathur/Fine-Grained-Clothing-Classification/blob/master/data/cloth_categories/models/stage-1_sz-150.pth?raw=true'
-model_file_name = 'model'
-classes = ['Blouse', 'Blazer', 'Button-Down', 'Bomber', 'Anorak', 'Tee', 'Tank', 'Top', 'Sweater', 'Flannel', 'Hoodie', 'Cardigan', 'Jacket', 'Henley', 'Poncho', 'Jersey', 'Turtleneck', 'Parka', 'Peacoat', 'Halter', 'Skirt', 'Shorts', 'Jeans', 'Joggers', 'Sweatpants', 'Jeggings', 'Cutoffs', 'Sweatshorts', 'Leggings', 'Culottes', 'Chinos', 'Trunks', 'Sarong', 'Gauchos', 'Jodhpurs', 'Capris', 'Dress', 'Romper', 'Coat', 'Kimono', 'Jumpsuit', 'Robe', 'Caftan', 'Kaftan', 'Coverup', 'Onesie']
+model_file_url = 'https://drive.google.com/uc?export=download&id=1-SjgZDA-5c4WgRGQggQgbqkZdyXEhod8'
+
+model_file_name = 'export.pkl'
+classes = ['bacterial_spot', 'early_blight', 'late_blight', 'leaf_mold', 'Septoria_leaf_spot', 'Two-spotted_spider_mites', 'target_spot', 'yello_leaf_curl_virus', 'mosaic_virus', 'healthy']
+
 
 path = Path(__file__).parent
 
